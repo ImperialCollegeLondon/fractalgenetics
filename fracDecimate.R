2e55ebcdd6bb22b7151ec7d64ee27c94e04fbff1
Hannah <hannah.v.meyer@gmail.com>
2018-08-29 17:16:47 +0000

    Check that reading from file works; verbose options; remove stats package

--- fracDecimate.R
+++ fracDecimate.R
@@ -1,19 +1,26 @@
 # Code to interpolate missing values and fit FDs to a set number of slices
 # Tim Dawes May 2018
 
-fracDecimate<- function (interpNoSlices=10, cut.off=3, data=NULL, filename=NULL,
-    id.col.name="Folder", interactive=FALSE) {
-    # Install stats package if not currently installed and then load
-    if("stats" %in% rownames(installed.packages()) == FALSE) {
-        install.packages("stats")
-    }
-    library(stats)
+fracDecimate <- function (interpNoSlices=10, cut.off=3, data=NULL, filename=NULL,
+    id.col.name="Folder", interactive=FALSE, verbose=TRUE) {
 
-    # private function
+    # private functions
     is.this.an.FD.value <- function(vec) {
         nonFD <- c("NA","NaN","Meagre blood pool", "Sparse myocardium",
             "FD measure failed")
         !(is.na(vec) | vec %in% nonFD)
+    }
+
+    interpolateSlices <- function(slices, interpNoSlices) {
+        xs.orig <- which(is.this.an.FD.value(slices))
+        ys.orig <- as.numeric(as.character(slices[xs.orig]))
+        tmp <- ksmooth(xs.orig, ys.orig, kernel="normal", bandwidth=1.5,
+                       range.x=range(xs.orig), n.points=interpNoSlices)
+        if(any(is.na(tmp$y))) {
+            return(rep(NA, interpNoSlices))
+        } else {
+            return(tmp$y)
+       }
     }
 
     # Error handling
@@ -34,14 +41,15 @@ fracDecimate<- function (interpNoSlices=10, cut.off=3, data=NULL, filename=NULL,
     }
     # Read in the FD data and pull out the columns to use for interpolation
     if (is.null(data)) {
-        data <- read.csv(file=filename, fill=TRUE, na.strings=c("NaN", "NA"))
+        data <- read.csv(file=filename, fill=TRUE, na.strings=c("NaN", "NA"),
+            stringsAsFactors=FALSE)
     }
     #FR.all<- data[,10:29]
-    FR.all <- data[, grepl("Slice \\d{1,2}", colnames(data))]
+    FR.all <- data[, grepl("Slice[\\.\\d]{1,2}$", colnames(data), perl=TRUE)]
     rownames(FR.all) <- data[,colnames(data) == id.col.name]
 
     # Work out how many viable slices are present in each subject
-    no.of.values <- colSums(apply(FR.all,1, is.this.an.FD.value))
+    no.of.values <- colSums(apply(FR.all, 1, is.this.an.FD.value))
 
     if (interactive) {
         hist(no.of.values, col="blue", xlab="No.of.slices/subject",
@@ -52,30 +60,17 @@ fracDecimate<- function (interpNoSlices=10, cut.off=3, data=NULL, filename=NULL,
     # Remove any subjects with fewer than a set number of FD values available for analysis
     # The threshold for this is set by the variable "cut-off"
     exclude <- round(100 * length(which(no.of.values < cut.off))/nrow(FR.all),1)
-    cat("\n \n Excluding ", exclude, "% subjects because they have <", cut.off,
-        " slices available. \n \n", sep="")
+    if (verbose) {
+        message(" Excluding ", exclude, "% subjects because they have <",
+            cut.off, " slices available.\n", sep="")
+    }
     FR.all <- FR.all[which(no.of.values >= cut.off),]
 
-    # Set up the output matrix
-    FRi <- matrix(0, nrow=nrow(FR.all), ncol=interpNoSlices,
-                  dimnames=list(rownames(FR.all),
-                  paste("Slice_", 1:10, sep="")))
-    if (interactive) head(FRi)
-
     # interpolate to a set number of slices per subject
-    interpolateSlices <- function(slices, interpNoSlices) {
-        xs.orig <- which(is.this.an.FD.value(slices))
-        ys.orig <- as.numeric(as.character(slices[xs.orig]))
-        tmp <- ksmooth(xs.orig, ys.orig, kernel="normal", bandwidth=1.5,
-                       range.x=range(xs.orig), n.points=interpNoSlices)
-        if(any(is.na(tmp$y))) {
-            return(rep(NA, interpNoSlices))
-        } else {
-            return(tmp$y)
-       }
+    if (verbose) {
+        message(" Interpolate slices to ", interpNoSlices, " for each of the ",
+            nrow(FR.all), " subjects\n")
     }
-    cat(" Interpolate slices to ", interpNoSlices, "for each of the",
-        nrow(FR.all), "subjects\n")
     FRi <- t(apply(FR.all, 1, interpolateSlices, interpNoSlices=interpNoSlices))
     dimnames(FRi) <- list(rownames(FR.all), paste("Slice_", 1:10, sep=""))
 
