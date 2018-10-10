52ee4fa69378ce369ca70526fce117983948508f
Hannah <hannah.v.meyer@gmail.com>
2018-10-10 12:50:23 +0000

    Depict genotypeQC.sh results and create QC-ed dataset

---
+++ QC/genotypeQC.R
@@ -0,0 +1,240 @@
+#############
+# libraries #
+#############
+
+options(import.path="/homes/hannah/RPackagesDevel/plinkQC")
+options(bitmapType = 'cairo', device = 'pdf')
+
+optparse <- modules::import_package('optparse')
+plinkqc <- modules::import_package('plinkqc')
+
+
+################
+### Analysis ###
+################
+
+## command line arguments ####
+option_list <- list(
+    optparse$make_option(c("-d", "--directory"), action="store",
+               dest="qcdir",
+               type="character", help="Path to directory with QC data/raw data
+               results [default: %default].", default=NULL),
+    optparse$make_option(c("-N", "--name"), action="store", dest="alg",
+               type="character", help="Prefix of plink-files i.e.
+               prefix.bim/prefix.bed/prefix.fam [default: %default].",
+               default=NULL),
+    optparse$make_option(c("--check_sex"), action="store_true",
+               dest="do.check_sex", help="Identify individuals with
+               discordant sex information (genetic sex/assigned sex)
+               [default: %default].", default=FALSE),
+    optparse$make_option(c("--maleTh"), action="store", dest="maleTh",
+               type="double", help="Threshold for homozygosity
+               rates expected on X-chromosome for males [default: %default].",
+               default=0.8),
+    optparse$make_option(c("--femaleTh"), action="store", dest="femaleTh",
+               type="double", help="Threshold for homozygosity
+               rates expected on X-chromosome for females [default: %default].",
+               default=0.2),
+    optparse$make_option(c("--fixMixup"), action="store_true",
+               dest="fixMixup", help="Should PEDSEX of individuals with
+               mismatch between PEDSEX and externalSexSex when
+               externalSexSex==SNPSEX automatically corrected: this will directly
+               change the alg.bim/.bed/.fam files! [default: %default].",
+               default=FALSE),
+    optparse$make_option(c("--externalSex"), action="store",
+               dest="externalSexFile",
+               type="character", help="/path/to/file/with/external/sex/id
+               containing sample IDs [externalSexID] and sex [externalSexSex]
+               to double check if external and PEDSEX data (often processed at
+               different centers) match. Only required if --fixMixup.
+               [default: %default].", default=NULL),
+    optparse$make_option(c("--externalSexID"), action="store",
+               dest="externalSexID",
+               type="character", help="Column identifier for column containing
+               ID information in externalSex [default: %default].",
+               default="IID"),
+    optparse$make_option(c("--externalSexSex"), action="store",
+               dest="externalSexSex",
+               type="character", help="Column identifier for column containing
+               sex information in externalSex [default: %default].",
+               default="IID"),
+    optparse$make_option(c("--externalMale"), action="store", dest="externalMale",
+               type="character", help="Identifier for 'male' in externalSexSex,
+               e.g. 1 or 'male' [default: %default].", default='M'),
+    optparse$make_option(c("--externalFemale"), action="store",
+               dest="externalFemale", type="character", help="Identifier for
+               'female' in externalSexSex file, e.g. 2 or 'female'
+               [default: %default].", default='F'),
+    optparse$make_option(c("--check_het_and_miss"), action="store_true",
+               dest="do.check_het_and_miss", help="Identify individuals
+               with outlying missing genotype or heterozygosity rates
+               [default: %default].", default=FALSE),
+    optparse$make_option(c("--imissTh"), action="store", dest="imissTh",
+               type="double", help="Threshold for acceptable missing genotype
+               rate per individual [default: %default].", default=0.03),
+    optparse$make_option(c("--hetTh"), action="store", dest="hteTh",
+               type="double", help="Threshold for acceptable heterozyosity rates;
+               any sample outside het*standard deviation(heterozygosity) will
+               be excluded [default: %default].", default=3),
+    optparse$make_option(c("--check_relatedness"), action="store_true",
+               dest="do.check_relatedness", help="Identify related
+               individuals [default: %default].", default=FALSE),
+    optparse$make_option(c("--filterRelated"), action="store_true",
+               dest="filterRelated", help="Set flag if  related
+               individuals should be removed from final dataset
+               [default: %default].", default=FALSE),
+    optparse$make_option(c("--relatednessTh"), action="store", dest="highIBDTh",
+               type="double", help="Identity be descent threshold, i.e.
+               threshold for filtering of relatedness [default: %default].",
+               default=0.1875),
+    optparse$make_option(c("--check_ancestry"), action="store_true",
+               dest="do.check_ancestry", help="Identify genetic
+               ancestry of individuals [default: %default].",
+               default=FALSE),
+    optparse$make_option(c("--europeanTh"), action="store", dest="europeanTh",
+               type="double", help=" Scaling factor of radius to be drawn around
+               centre of reference European samples, with study samples inside
+               this radius considered to be of European descent and samples
+               outside this radius of non-European descent. The radius is
+               computed as the maximum Euclidean distance of reference Europeans
+               to the centre of reference European samples [default: %default].",
+               default=1.5),
+    optparse$make_option(c("--prefixMerged"), action="store", dest="prefixMerged",
+               help="Prefix of merged dataset (study and reference samples) used
+               in plink --pca, resulting in prefixMerged.eigenvec [default:
+               %default].", default=NULL),
+    optparse$make_option(c("--refSamples"), action="store",
+              dest="refSamplesFile", type="character", help="/path/to/file with
+              sample identifiers [refSamplesIID] corresponding to reference IIDs
+              in prefixMerged.eigenvec and population identifier [refSamplesPop]
+              corresponding to population IDs [refColorsPop] in refColors.
+              [default: %default].", default=NULL),
+    optparse$make_option(c("--refColors"), action="store",
+              dest="refColorsFile", type="character", help="/path/to/file with
+              population IDs in column [refColorsPop] and corresponding color-
+              code for PCA plot in column [refColorsColor] [default: %default].",
+              default=NULL),
+    optparse$make_option(c("--refSamplesIID"), action="store",
+              dest="refSamplesIID", type="character", help="Column name of
+              reference sample IDs in --refSamples [default: %default].",
+              default="IID"),
+    optparse$make_option(c("--refSamplesPop"), action="store",
+              dest="refSamplesPop", type="character", help="Column name of
+              reference sample population IDs in --refSamples [default:
+              %default].", default="Pop"),
+    optparse$make_option(c("--refColorsColor"), action="store",
+              dest="refColorsColor", type="character", help="Column name of
+              population colors in --refColors [default: %default].",
+              default="Color"),
+    optparse$make_option(c("--refColorsPop"), action="store",
+              dest="refColorsPop", type="character", help="Column name of
+              reference sample population in --refColors [default:
+              %default].", default="Color"),
+    optparse$make_option(c("--studyColor"), action="store",
+              dest="studyColor", type="character", help="Colour to be used for
+              study population if PCA plot [default: %default].",
+              default="#2c7bb6"),
+    optparse$make_option(c("--lmissTh"), action="store", dest="lmissTh",
+               type="double", help="Threshold for acceptable missing genotype
+               rate per marker [default: %default].", default=0.01),
+    optparse$make_option(c("--hweTh"), action="store", dest="hweTh",
+               type="double", help="Threshold for considering deviation from
+               Hardy-Weinberg equilibrium significant [default: %default].",
+               default=1e-5),
+    optparse$make_option(c("--mafTh"), action="store", dest="mafTh",
+               type="double", help="Minor allele frequency treshold [default:
+               %default].", default=0.01),
+    optparse$make_option(c("--macTh"), action="store", dest="macTh",
+               type="integer", help="Minor allele count treshold. If both --maf
+               and --mac are supplied, --maf will be considered [default:
+               %default].", default=20),
+    optparse$make_option(c("--showProgress"), action="store_true",
+               dest="verbose",
+               default=FALSE, type="logical", help="If set, progress messages
+               about analyses are printed to standard out [default: %default]."),
+    optparse$make_option(c("-p", "--plot"), action="store_true",
+               dest="plot", help="Plot QC results [default: %default].",
+               default=FALSE),
+    optparse$make_option(c("--plink"), action="store", dest="path2plink",
+               help="Absolute path to where external plink software
+               (https://www.cog-genomics.org/plink/1.9/) can be found. If not
+               provided, assumed that PATH set-up works and plink will be found
+               by system('plink'). [default: %default].", default=NULL)
+)
+
+args <- optparse$parse_args(optparse$OptionParser(option_list=option_list))
+
+if (FALSE) {
+    args <- list()
+    args$alg <- "gencall.sanger12"
+    args$qcdir <- "/homes/hannah/data/genotype/QC/sanger12"
+    args$maleTh <- 0.8
+    args$femaleTh <- 0.2
+    args$imissTh <- 0.03
+    args$lmissTh <- 0.01
+    args$hetTh <- 3
+    args$highIBDTh <- 0.1875
+    args$hweTh <- 1e-5
+    args$mac <- 20
+    args$verbose <- TRUE
+    args$plot <- TRUE
+    args$sample <- "~/data/phenotype/2Dphenotype/20160209_All_BRU_family_format.txt"
+    args$refSamplesFile="~/data/hmeyer/HapMap/HapMap_ID2Pop.txt"
+    args$refColorsFile="~/data/hmeyer/HapMap/HapMap_PopColors.txt"
+    args$externalSexID="Bru.Number"
+    args$prefixMergedDataset="gencall.sanger12.HapMapIII"
+}
+
+
+if (args$plot) pdf(paste(args$qcdir,"/", args$alg,".pdf",sep=""), width=10,
+                   height=8)
+## Per-individual QC ####
+if (args$verbose) message("per-individual QC")
+SampleID <- read.table(file=args$sample, sep="\t", header=TRUE, quote="",
+                           stringsAsFactors=FALSE, na.strings=c("NA",""))
+fail_individuals <-
+    plinkqc$perSampleQC(qcdir=args$qcdir, alg=args$alg,
+                        do.check_sex=args$do.check_sex,
+                        maleTh=args$maleTh,
+                        femaleTh=args$femaleTh,
+                        externalSex=SampleID,
+                        fixMixUp=args$fixMixUp,
+                        externalMale=args$externalMale,
+                        externalFemale=args$externalFemale,
+                        externalSexSex=args$externalSexSex,
+                        externalSexID=args$externalSexID,
+                        do.check_heterozygosity_and_missingess=
+                            args$do.check_het_and_miss,
+                        imissTh=args$imissTh,
+                        hetTh=args$hetTh,
+                        do.check_relatedness=args$do.check_relatedness,
+                        highIBDTh=args$highIBDTh,
+                        do.check_ancestry=args$do.check_ancestry,
+                        prefixMergedDataset=args$prefixMerged,
+                        refSamplesFile=args$refSamplesFile,
+                        refColorsFile=args$refColorsFile,
+                        refSamplesIID=args$refSamplesIID,
+                        refSamplesPop=args$refSamplesPop,
+                        refColorsColor=args$refColorsColor,
+                        refColorsPop=args$refColorsPop,
+                        studyColor=args$studyColor,
+                        path2plink=args$path2plink,
+                        interactive=args$plot, verbose=args$verbose)
+
+overview <- plinkqc$overviewPerSampleQC(fail_individuals, interactive=args$plot)
+
+## Per-marker QC ####
+if (args$verbose) message("per-marker QC")
+fail_markers <- plinkqc$perMarkerQC(qcdir=args$qcdir, alg=args$alg,
+                                    mafTh=args$mafTh, macTh=args$macTh,
+                                    hweTh=args$hweTh, lmissTh=0.01,
+                                    path2plink=args$path2plink,
+                                    interactive=args$plot, verbose=args$verbose)
+if (args$plot) dev.off()
+
+## Create QCed dataset ####
+if (args$verbose) message("Clean data")
+plinkqc$clean_data(qcdir=args$qcdir, alg=args$alg, mafTh=args$mafTh,
+                          macTh=args$macTh, hweTh=args$hweTh, lmissTh=0.01,
+                          path2plink=args$path2plink,
+                          filterRelated=args$filterRelated, verbose=args$verbose)
