739dcf78abbc9534b07b1c9d80eeb1ca50e54ee7
Hannah <hannah.v.meyer@gmail.com>
2018-10-10 14:22:02 +0000

    Check probes on different genotyping arrays

--- chipQC/compare_probeID_across_batches.R
+++ chipQC/compare_probeID_across_batches.R
@@ -11,24 +11,24 @@
 ###################################################
 
 
-#################
-### analysis  ###
-#################
+#############
+### data  ###
+#############
 
-directory <- "/homes/hannah/GWAS/data/genotype"
+directory <- "~/data/genotype"
 
-### 1. SNP info data for the different platforms/batches
+## 1. SNP info data for the different platforms/batches ####
 # Sanger batch 1 and 2
 sanger <- read.table(paste(directory,
-                           "omnix_hhtmri_20141121/HumanOmniExpress-12v1-1_A.csv",
+                           "/omnix_hhtmri_20141121/HumanOmniExpress-12v1-1_A.csv",
                            sep=""), sep=",", header=TRUE, skip=7, fill=TRUE,
                      stringsAsFactors=FALSE)
 sanger <- sanger[!is.na(sanger$AddressA_ID),]
 sanger_chip <- "HumanOmniExpress-12v1-1_A"
 
-# Singapore combined batch 1 and 2, and batch
+# Singapore combined batch 1 and 2, and batch 3
 singapore12 <- read.table(paste(directory,
-                                "omnix_NHCS_20160217/Manifests/",
+                                "/omnix_NHCS_20160217/Manifests/",
                                 "HumanOmniExpress-24v1-0/",
                                 "humanomniexpress-24v1-0_a.csv", sep=""),
                           sep=",", header=TRUE, skip=7, fill=TRUE,
@@ -44,32 +44,32 @@ singapore3 <- read.table(paste(directory, "/omnix_NHCS_20160217/Manifests/",
 singapore3 <- singapore3[!is.na(singapore3$AddressA_ID),]
 singapore3_chip <-"HumanOmniExpress-24v1-1_A"
 
-ID_overlap <- VennDiagram::calculate.overlap(
-                                    list("Duke-NUS12" =singapore12[,"Name"],
-                                         "Duke-NUS3"=singapore3[,"Name"],
-                                          Sanger12=sanger[,"Name"]))
-venn.plot <- VennDiagram::venn.diagram(list("Duke-NUS12" =singapore12[,"Name"],
-                               "Duke-NUS3"=singapore3[,"Name"],
-                               "Sanger12" =sanger[,"Name"]),
-                          filename=paste(directory,
-                                         "/Venn_genotyping_batches.pdf", sep=""),
-                          imagetype="pdf",
-                          category.names=
-                              c("Duke-NUS12\nHumanOmniExpress-24v1-0",
-                                "Duke-NUS3\nHumanOmniExpress-24v1-1_A",
-                                "Sanger12\nHumanOmniExpress-12v1-1_A"),
-                          cat.pos = c(-20, 0, 25),
-                          cat.dist = c(0.08, 0.08, 0.02),
-                          cat.col = c("darkorchid2","darkblue", "darkgreen"),
-                          cat.cex=0.8
-                          )
+#################
+### analysis  ###
+#################
+
+ID_list <- list("Duke-NUS12"=singapore12$Name, "Duke-NUS3"=singapore3$Name,
+                                          "Sanger12"=sanger$Name)
+
+pdf(paste(directory, "/overlap_genotypeProbes.pdf", sep=""))
+UpSetR::upset(UpSetR::fromList(ID_list),
+              order.by = "freq",
+              empty.intersections = "on", text.scale=1.2,
+              #scale.sets="log10", scale.intersections="log10",
+              # Include when UpSetR v1.4.1 is released
+              # title="Overview quality control failures",
+              main.bar.color="#1b9e77", matrix.color="#1b9e77",
+              sets.x.label="Number of probes",
+              mainbar.y.label="Number of common probes",
+              sets.bar.color="#d95f02")
+dev.off()
 
 singapore <- merge(singapore12, singapore3, by="Name")
 
 # merge based on rsIDs
-all <- merge(sanger, singapore, by="Name")[, c("Name", "AlleleA_ProbeSeq",
-                                               "AlleleA_ProbeSeq.x",
-                                               "AlleleA_ProbeSeq.y")]
+all <- dplyr::select_(merge(sanger, singapore, by="Name"),
+                      ~Name, ~AlleleA_ProbeSeq, ~AlleleA_ProbeSeq.x,
+                      ~AlleleA_ProbeSeq.y)
 
 # all SNPs with same sequence ID
 all_seq_index <- apply(all, 1, function(x) {
@@ -78,6 +78,7 @@ all_seq_index <- apply(all, 1, function(x) {
 all_seq <- all[all_seq_index,]
 
 ### -> SNPs common to all platforms have same probes, check!
-
+nrow(all_seq) == nrow(all)
+# [1] TRUE
 
 
