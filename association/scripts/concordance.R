3b8f4daf31ca9fe9a16dba576107795331728412
Hannah <hannah.v.meyer@gmail.com>
2018-11-05 07:12:39 +0000

    Format; add slices to dh dataframe

--- association/scripts/concordance.R
+++ association/scripts/concordance.R
@@ -51,41 +51,38 @@ write.table(slices_dh,
                        sep=""),
             sep=",",quote=FALSE, col.names=TRUE, row.names=FALSE)
 
-#slices_dh <- data.table::fread(paste(directory,
-#                                     "/bgenie_slices_lm_st_genomewide.csv",
-#                                     sep=""),
-#                               data.table=FALSE, stringsAsFactors=FALSE)
-
 
 ## ld-filtered, significant genome-wide association results ukb ####
 slices_ukb <- read.table(paste(dirukb, "/Slices_sig5e08_ldFiltered.txt", sep=""),
                          sep=",", stringsAsFactors=FALSE, header=TRUE)
 
-## format ukb betas and p-values per slice
+## format ukb betas and p-values per slice ####
 slices_ukb_beta <- cbind(rsid=slices_ukb$rsid,
                           slices_ukb[,grepl('beta', colnames(slices_ukb))])
 colnames(slices_ukb_beta) <- gsub("_beta", "", colnames(slices_ukb_beta))
-beta <- reshape2::melt(slices_ukb_beta, id.var='rsid', value.name='beta')
+beta <- reshape2::melt(slices_ukb_beta, id.var='rsid', value.name='beta',
+                       variable.name='slice')
 
 slices_ukb_logp <- cbind(rsid=slices_ukb$rsid,
                           slices_ukb[, grepl('log10p', colnames(slices_ukb))])
 colnames(slices_ukb_logp) <- gsub("\\.log10p", "", colnames(slices_ukb_logp))
-logp <- reshape2::melt(slices_ukb_logp, id.var='rsid', value.name='logp')
+logp <- reshape2::melt(slices_ukb_logp, id.var='rsid', value.name='logp',
+                       variable.name='slice')
 
 ukb <- cbind(beta, logp=logp$logp)
 
-## ukb significant slice pvalues and betas
+## ukb significant slice pvalues and betas ####
 ukb <- ukb[ukb$rsid %in% slices_dh$rsid, ]
 ukb_sig_slices <- dplyr::filter(ukb, logp  > -log10(5e-8))
 
-slices2sample <- as.data.frame(table(ukb_sig_slices$variable),
+slices2sample <- as.data.frame(table(ukb_sig_slices$slice),
                                stringsAsFactors=FALSE)
 colnames(slices2sample) <- c("slice", "freq")
 slices2sample <- slices2sample[slices2sample$freq != 0,]
 
 observedBetas <- ukb_sig_slices$beta
 
-## betas and pvalues of significant snps and slice in ukb
+## betas and pvalues of significant snps and slice in ukb ####
 dh_beta <- slices_dh[,c(2, which(grepl('beta', colnames(slices_dh))))]
 colnames(dh_beta) <- gsub("_beta", "", colnames(dh_beta))
 dh_logp <- slices_dh[,c(2, which(grepl('log10p', colnames(slices_dh))))]
@@ -95,15 +92,16 @@ dh_sig_slices <- data.frame(t(apply(ukb_sig_slices, 1, function(x) {
     pos <- which(dh_beta$rsid %in%  x[1])
     beta_tmp <- dh_beta[pos, colnames(dh_beta) %in% x[2]]
     logp_tmp <- dh_logp[pos, colnames(dh_logp) %in% x[2]]
-    return(rbind(beta_tmp, logp_tmp))
-})))
-colnames(dh_sig_slices) <- c("beta", "logp")
+    return(rbind(x[2], beta_tmp, logp_tmp))
+})), stringsAsFactors=FALSE)
+colnames(dh_sig_slices) <- c("slice", "beta", "logp")
 dh_sig_slices$rsid <- ukb_sig_slices$rsid
+dh_sig_slices <- dplyr::select(dh_sig_slices, rsid, slice, beta, logp)
 
-## concordance of effect sizes of significant snps betwenn ukb and digital-heart
+## concordance of effect sizes ####
 observedConcordance <- sum(sign(dh_sig_slices$beta * observedBetas))
 
-## Empirical concordance: matched for number of sig snps and slices
+## Empirical concordance: matched for number of sig snps and slices ####
 nrsig <- nrow(ukb_sig_slices)
 nrtotal <- nrow(slices_dh)
 draws <- 100000
@@ -124,7 +122,7 @@ empiricalConcordance <-
     length(which(testConcordance >= observedConcordance))/draws
 
 ## plot beta concordance ukb and digital heart ####
-slices <- cbind(ukb_sig_slices, dh_sig_slices[, -3])
+slices <- cbind(ukb_sig_slices, dh_sig_slices[,3:4])
 colnames(slices) <- c('rsid', 'slices', 'ukbb_beta', 'ukbb_logp', 'dh_beta',
                       'dh_logp')
 slices$sig <- factor(as.numeric(slices$dh_logp > -log10(0.005)),
