d9ccba2442e65fd57e927a827a06134dc9b97955
HannahVMeyer <hannah.v.meyer@googlemail.com>
2019-10-17 15:48:44 -0400

    select proper indeces

--- UK-Biobank/association/mendelian-randomisation.R
+++ UK-Biobank/association/mendelian-randomisation.R
@@ -309,8 +309,8 @@ access <- token$credentials$access_token
 
 #SV ('UKB-b:6025'), QRS duration ('UKB-b:2240'), HR ('1056'), SBP ('UKB-a:360')
 # DBP ('UKB-a:359')
-outcomes <- c('UKB-b:6025', 'UKB-b:2240', 'UKB-a:359', 'UKB-a:360')
-names(outcomes) <- c("SV", "QRS", "DBP", "SBP")
+outcomes <- c('UKB-b:6025', 'UKB-b:2240', 'UKB-a:360')
+names(outcomes) <- c("SV", "QRS", "SBP")
 
 basalMRbase <- getMRdata(data_exposure=exposure_per_area[[1]],
                        outcomes=as.vector(outcomes), access=access)
@@ -322,19 +322,9 @@ apicalMRbase <- getMRdata(data_exposure=exposure_per_area[[3]],
 MRbase <- list(basal=basalMRbase, mid=midMRbase, apical=apicalMRbase)
 saveRDS(MRbase, paste(directory, "/MR/MRbase.rds", sep=""))
 
-
-MRbase <- lapply(MRbase, function(x) {
-    #x$exposure <-x$exposure[x$exposure$SNP != "rs117953218",]
-    x$outcome <-x$outcome[x$outcome$SNP != "rs117953218",]
-    #x$outcome$beta.outcome <- x$outcome$beta.outcome * (-1)
-    return(x)
-})
-
 MRbase_results <- lapply(MRbase, function(x) {
     MRanalysis(x$exposure, x$outcome)
 })
-
-
 
 mr_tables <- lapply(seq_along(MRbase_results), function(x) {
     region <- MRbase_results[[x]]
@@ -368,12 +358,12 @@ panel_plots <- lapply(mr_plots, function(x) x$all_plots)
 
 #SBP ('UKB-a:360'), SV ('UKB-b:6025'), QRS duration ('UKB-b:2240')
 
-panels_sbp <- lapply(panel_plots, function(x) x[[3]])
+panels_sbp <- lapply(panel_plots, function(x) x[[1]])
 p_panels_sbp <- cowplot::plot_grid(plotlist=panels_sbp, nrow=3)
 ggsave(plot=p_panels_sbp, paste(directory, "/MR/MR_panels_SBP.pdf", sep=""),
        height=20, width=12)
 
-panels_qrs <- lapply(panel_plots, function(x) x[[3]])
+panels_qrs <- lapply(panel_plots, function(x) x[[2]])
 p_panels_qrs <- cowplot::plot_grid(plotlist=panels_qrs, nrow=3)
 ggsave(plot=p_panels_qrs, paste(directory, "/MR/MR_panels_QRS.pdf", sep=""),
        height=20, width=12)
@@ -384,15 +374,15 @@ ggsave(plot=p_panels_sv, paste(directory, "/MR/MR_panels_SV.pdf", sep=""),
        height=20, width=12)
 
 
-forrests_sbp <- lapply(mr_plots, function(x) x$forrest[[3]] + theme_bw() +
-                           scale_x_continuous(limits=c(-1,4)) +
+forrests_sbp <- lapply(mr_plots, function(x) x$forrest[[1]] + theme_bw() +
+                           scale_x_continuous(limits=c(-2,4)) +
                            theme(legend.position='none',
                                  axis.title.x = element_blank()))
 p_forrests_sbp <- cowplot::plot_grid(plotlist=forrests_sbp, ncol=3)
 ggsave(plot=p_forrests_sbp, paste(directory, "/MR/MR_forrest_SBP.pdf", sep=""),
        height=4, width=12)
 
-p_basal_forrest_sbp <- mr_plots[[1]]$forrest[[3]] +
+p_basal_forrest_sbp <- mr_plots[[1]]$forrest[[1]] +
     theme_bw() +
     theme(legend.position='none')
 
@@ -401,7 +391,7 @@ ggsave(plot=p_basal_forrest_sbp, paste(directory,
        height=4, width=4)
 
 forrests_qrs <- lapply(mr_plots, function(x) x$forrest[[2]] + theme_bw() +
-                           scale_x_continuous(limits=c(-1,4)) +
+                           scale_x_continuous(limits=c(-2,4)) +
                            theme(legend.position='none',
                                  axis.title.x = element_blank()))
 p_forrests_qrs <- cowplot::plot_grid(plotlist=forrests_qrs, ncol=3)
@@ -409,7 +399,7 @@ ggsave(plot=p_forrests_qrs, paste(directory, "/MR/MR_forrest_QRS.pdf", sep=""),
        height=4, width=12)
 
 forrests_sv <- lapply(mr_plots, function(x) x$forrest[[3]] + theme_bw() +
-                          scale_x_continuous(limits=c(-1,4)) +
+                          scale_x_continuous(limits=c(-2,4)) +
                           theme(legend.position='none',
                               axis.title.x = element_blank()))
 p_forrests_sv <- cowplot::plot_grid(plotlist=forrests_sv, ncol=3)
