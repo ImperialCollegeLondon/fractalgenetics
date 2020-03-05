c74b50e85eaae7828326808430d2cc3de3d7d946
HannahVMeyer <hannah.v.meyer@googlemail.com>
2020-03-05 15:25:44 -0500

    Move FD QRS analysis

--- UK-Biobank/phenotypes/FD_longQRS.R
+++ UK-Biobank/phenotypes/FD_longQRS.R
@@ -4,7 +4,8 @@
 longQRS<- which(dataI$ECG_QRS_DURATION>120)
 dataI$longQRS_binary<- rep(0, nrow(dataI))
 dataI$longQRS_binary[longQRS]<- 1
-fit<- glm(longQRS_binary ~ scale(MEANGLOBALFD), data=dataI, family=binomial(link='logit'))
+fit<- glm(longQRS_binary ~ scale(MEANGLOBALFD), data=dataI,
+          family=binomial(link='logit'))
 
 exp(fit$coefficients)
 exp(confint(fit))
